d9c5275024207f0275b3f53127409ef69724ba35
kolapp <shitpoppah@hotmail.com>
2015-04-21 16:23:02 +0200

    Doksi - csak tartalomjegyzék, szerkezet készül

---
+++ _Doksi/vazlat.c
@@ -0,0 +1,43 @@
+a) első kötéstábla
+b) címoldal 
+
+
+c) feladatkiírás
+	- Robi írja?
+	
+d) tartalmi összefoglaló
+	- oldal mese
+	
+e) tartalomjegyzék
+
+f) érdemi rész
+/* --------- felhasznált módszerek, eszközök ----*/
+	2. Felhasznált módszerek, eszközök
+		- következő fejezetekhez összefoglaló
+		- specifikáció
+		- hardver / szoftverfejlesztés 
+/* --------- a saját elvégzett munka ----------- */
+	3. Átviteli függvény mérés
+		- Elmélet
+		- Szűrő panel
+		- Source
+		- Labview
+	4. Ház fűtés szimuláció
+		- HIL leírás
+		- 
+/* -------- az eredmények tézispontszerű ismertetése ------ */
+	5. Mérési eredmények
+	
+g) irodalomjegyzék
+	- weboldalak, könyvek?, cégek
+
+	
+h) nyilatkozat
+i) köszönetnyilvánítás
+j) melléletek
+k) hátsó kötéstábla
+
+
+
+
+
